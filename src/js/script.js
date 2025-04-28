f40b50b8fcd508b3f7784d7459996ba65ee24a69
Denis <denis@gmail.com>
2025-04-28 18:43:40 +0300

    added modal form

--- src/js/script.js
+++ src/js/script.js
@@ -33,22 +33,55 @@ $(document).ready(function () {
 
     $('.slider__wrapper').on('afterChange', updateArrows);
 
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav_media');
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav_active');
-        burger.classList.toggle('burger_active');
-    });
+});
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            }
-        });
-    });
 
-    document.querySelectorAll('.menu__list').forEach(el => {
-        observer.observe(el);
+// burger
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav_media');
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav_active');
+    burger.classList.toggle('burger_active');
+});
+
+
+//animation when scroll
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
     });
 });
+
+document.querySelectorAll('.menu__list').forEach(el => {
+    observer.observe(el);
+});
+
+//modal
+
+const btn = document.querySelectorAll('.button');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.modal__close');
+btn.forEach((item) => {
+    item.addEventListener('click', () => {
+        // modal.style.display = 'flex';
+        overlay.style.opacity = '1';
+        overlay.style.visibility = 'visible';
+    });
+});
+
+close.addEventListener('click', () => {
+    // modal.style.display = 'none';
+    overlay.style.opacity = '0';
+    overlay.style.visibility = 'hidden';
+});
+
+//form validation
+
+
+
+// function validate(){
+    
+// }
